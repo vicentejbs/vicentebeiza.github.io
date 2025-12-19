67f24cfedd8520d4c150eb072036639765c2ffd7
vicente beiza dell <vicente.beiza@usm.cl>
2025-12-19 00:50:35 -0300

    Fix: Volver a base subdirectorio para GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/",
+  base: "/vicentebeiza.github.io/",
   server: {
     host: "::",
     port: 8080,
